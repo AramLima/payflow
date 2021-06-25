2bd13cafa399df7069dced867bcc9040fb8f0fb8
Aram Lima <lima_aragorn@hotmail.com>
2021-06-25 08:51:30 -0300

    adicionando modulos e widgets

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -11,8 +11,8 @@ class SlpashPage extends StatelessWidget {
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
-          Image.asset(AppImages.union),
-          Image.asset(AppImages.logoFull),
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull)),
         ],
       ),
     );
